7f25656bc43b72ed3f5a5762e06fc975df095ed8
Ben Biddington <ben.biddington@gmail.com>
2013-09-19 20:54:08 +1200

    The scheme and authority must be lower-cased and query is not included

--- Wiki.Machinery/SignatureBaseStringFlowFixture.cs
+++ Wiki.Machinery/SignatureBaseStringFlowFixture.cs
@@ -29,7 +29,19 @@ namespace Wiki.Machinery {
 		}
 
 		protected string Value {
-			get { return _url; }
+			get { return new EarlPart(new Uri(_url)).Value; }
+		}
+	}
+
+	internal class EarlPart {
+		private readonly Uri _uri;
+
+		public EarlPart(Uri uri) {
+			_uri = uri;
+		}
+
+		public string Value {
+			get { return string.Format("{0}://{1}", _uri.Scheme, _uri.Authority); }
 		}
 	}
 
