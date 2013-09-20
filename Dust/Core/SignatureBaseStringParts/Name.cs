f6dc1d2bef41f17ee5a8cc2ea525e32e9f92453c
Ben Biddington <ben.biddington@gmail.com>
2013-09-20 16:42:29 +1200

    class => file

---
+++ Dust/Core/SignatureBaseStringParts/Name.cs
@@ -0,0 +1,29 @@
+namespace Dust.Core.SignatureBaseStringParts
+{
+    class Name
+    {
+        private readonly string _value;
+
+        internal static Name Version = new Name("version");
+        internal static Name ConsumerKey = new Name("consumer_key");
+        internal static Name Token = new Name("token");
+        internal static Name SignatureMethod = new Name("signature_method");
+        internal static Name Timestamp = new Name("timestamp");
+        public static Name Nonce = new Name("nonce");
+
+        internal Name(string value)
+        {
+            _value = string.Format("oauth_{0}", value);
+        }
+
+        internal string Value
+        {
+            get { return _value; }
+        }
+
+        public static implicit operator string(Name what)
+        {
+            return what.Value;
+        }
+    }
+}
--- Dust/Core/SignatureBaseStringParts/OAuthParameters.cs
+++ Dust/Core/SignatureBaseStringParts/OAuthParameters.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Dust.Core.SignatureBaseStringParts
 {
@@ -39,33 +38,6 @@ namespace Dust.Core.SignatureBaseStringParts
                 new Parameter(Name.Timestamp, _timestamp),
                 new Parameter(Name.Nonce, _nonce)
             };
-        }
-    }
-
-    class Name
-    {
-        private readonly string _value;
-
-        internal static Name Version = new Name("version");
-        internal static Name ConsumerKey = new Name("consumer_key");
-        internal static Name Token = new Name("token");
-        internal static Name SignatureMethod = new Name("signature_method");
-        internal static Name Timestamp = new Name("timestamp");
-        public static Name Nonce = new Name("nonce");
-
-        internal Name(string value)
-        {
-            _value = string.Format("oauth_{0}", value);
-        }
-
-        internal string Value
-        {
-            get { return _value; }
-        }
-
-        public static implicit operator string(Name what)
-        {
-            return what.Value;
         }
     }
 }
