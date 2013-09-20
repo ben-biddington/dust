abef9a54d213569810f3ccf3b41642fc8dc9e3a2
Ben Biddington <ben.biddington@gmail.com>
2013-09-21 11:27:54 +1200

    [WIP] [FAILS] Parameters are sorted using lexicographical byte value ordering

--- Dust/Core/SignatureBaseStringParts/ParameterComparison.cs
+++ Dust/Core/SignatureBaseStringParts/ParameterComparison.cs
@@ -7,16 +7,19 @@ namespace Dust.Core.SignatureBaseStringParts
     {
         public int Compare(Parameter x, Parameter y)
         {
-            int result = string.Compare(x.Name, y.Name, StringComparison.InvariantCultureIgnoreCase);
+            int result = CompareCore(x.Name, y.Name);
 
             var sameName = result == 0;
 
             return sameName ? CompareValues(x,y) : result;
         }
 
-        private int CompareValues(Parameter x, Parameter y)
-        {
-            return string.Compare(x.Value, y.Value, StringComparison.InvariantCultureIgnoreCase);
+        private int CompareValues(Parameter x, Parameter y) {
+        	return CompareCore(x.Value, y.Value);
         }
+
+    	private int CompareCore(string x, string y) {
+    		return string.Compare(x, y, StringComparison.InvariantCulture);
+    	}
     }
 }
