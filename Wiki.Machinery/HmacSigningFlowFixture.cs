369094c50bf1b763acaf6ff02bbb7ea867749917
Ben Biddington <ben.biddington@gmail.com>
2013-09-21 21:52:29 +1200

    Refactor: Composed method

--- Wiki.Machinery/HmacSigningFlowFixture.cs
+++ Wiki.Machinery/HmacSigningFlowFixture.cs
@@ -35,13 +35,23 @@ namespace Wiki.Machinery {
 		public string Sign(string signatureBaseString, string consumerSecret, string tokenSecret) {
 			var key = string.Format("{0}&{1}", consumerSecret, tokenSecret);
 
+			var signature = SignCore(signatureBaseString, key);
+
+			return Base64Encode(signature);
+		}
+
+		private string Base64Encode(byte[] bytes) {
+			return Convert.ToBase64String(bytes);
+		}
+
+		private byte[] SignCore(string text, string key) {
 			var encoding = Encoding.ASCII;
 
 			byte[] keyBytes = encoding.GetBytes(key);
-			byte[] textBytes = encoding.GetBytes(signatureBaseString);
+			byte[] textBytes = encoding.GetBytes(text);
 
 			using (var hmac = new HMACSHA1(keyBytes)) {
-				return Convert.ToBase64String(hmac.ComputeHash(textBytes));
+				return hmac.ComputeHash(textBytes);
 			}
 		}
 	}
