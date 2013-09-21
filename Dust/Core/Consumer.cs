3d37f591129458e64b28ae737bc2d9fe30c68170
Ben Biddington <ben.biddington@gmail.com>
2013-09-21 22:05:07 +1200

    Refactor: class => lib/

---
+++ Dust/Core/Consumer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Dust.Core {
+	public class Consumer {
+		public string Sign(string signatureBaseString, string consumerSecret, string tokenSecret) {
+			string key = Key(consumerSecret, tokenSecret);
+
+			var signature = SignCore(signatureBaseString, key);
+
+			return Base64Encode(signature);
+		}
+
+		private string Key(string consumerSecret, string tokenSecret) {
+			return string.Format("{0}&{1}", Escape(consumerSecret), Escape(tokenSecret));
+		}
+
+		private string Escape(string what) {
+			return Uri.EscapeDataString(what);
+		}
+
+		private string Base64Encode(byte[] bytes) {
+			return Convert.ToBase64String(bytes);
+		}
+
+		private byte[] SignCore(string text, string key) {
+			var encoding = Encoding.ASCII;
+
+			byte[] keyBytes = encoding.GetBytes(key);
+			byte[] textBytes = encoding.GetBytes(text);
+
+			using (var hmac = new HMACSHA1(keyBytes)) {
+				return hmac.ComputeHash(textBytes);
+			}
+		}
+	}
+}
--- Wiki.Machinery/HmacSigningFlowFixture.cs
+++ Wiki.Machinery/HmacSigningFlowFixture.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Security.Cryptography;
-using System.Text;
+﻿using Dust.Core;
 using fit;
 using fitlibrary;
 
@@ -28,39 +26,6 @@ namespace Wiki.Machinery {
 			return new YesNoFixture(expected == actual, 
 				"Invalid signature. Expected [" +  expected + "]. Got [" + actual + "].", 2
 			);
-		}
-	}
-
-	internal class Consumer {
-		public string Sign(string signatureBaseString, string consumerSecret, string tokenSecret) {
-			string key = Key(consumerSecret, tokenSecret);
-
-			var signature = SignCore(signatureBaseString, key);
-
-			return Base64Encode(signature);
-		}
-
-		private string Key(string consumerSecret, string tokenSecret) {
-			return string.Format("{0}&{1}", Escape(consumerSecret), Escape(tokenSecret));
-		}
-
-		private string Escape(string what) {
-			return Uri.EscapeDataString(what);
-		}
-
-		private string Base64Encode(byte[] bytes) {
-			return Convert.ToBase64String(bytes);
-		}
-
-		private byte[] SignCore(string text, string key) {
-			var encoding = Encoding.ASCII;
-
-			byte[] keyBytes = encoding.GetBytes(key);
-			byte[] textBytes = encoding.GetBytes(text);
-
-			using (var hmac = new HMACSHA1(keyBytes)) {
-				return hmac.ComputeHash(textBytes);
-			}
 		}
 	}
 }
