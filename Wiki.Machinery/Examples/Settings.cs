1d61b61da39fb5eddf63c0b7e7dfce6a226a917c
Ben Biddington <ben.biddington@gmail.com>
2013-09-24 13:56:11 +1200

    Fails unless the settings are defined

--- Wiki.Machinery/Examples/Settings.cs
+++ Wiki.Machinery/Examples/Settings.cs
@@ -1,17 +1,22 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using Dust;
+using Dust.Lang;
 
 namespace Wiki.Machinery.Examples {
 	internal static class Settings {
 		internal static Consumer Twitter {
-			get {
-				var settings = ConfigurationManager.AppSettings;
-				
-				return new Consumer(
-					new ConsumerKey(settings["twitter.consumer.key"]), 
-					settings["twitter.consumer.secret"]
-				);
-			}
-		}	
+			get { return new Consumer(new ConsumerKey(Key), Secret); }
+		}
+
+		private static string Key { get { return SettingFor("twitter.consumer.key"); } }
+		private static string Secret { get { return SettingFor("twitter.consumer.secret"); } }
+
+		private static string SettingFor(string name) {
+			return ConfigurationManager.AppSettings[name].Tap(it => {
+				if (null == it)
+	                throw new Exception("Unable to locate appsetting for [" + name + "]. Do you have an App.config?");                                  	
+			});
+		}
 	}
 }
